ec7c58a02178379980eea28c50401801a14032a4
Ashraf-Raneem <ashrafraneem@gmail.com>
2023-03-30 17:39:17 -0600

    navbars added

--- src/components/navbars/PrimaryNav.js
+++ src/components/navbars/PrimaryNav.js
@@ -5,26 +5,31 @@ const PrimaryNav = () => {
     const [searchValue, setSearchValue] = useState("");
 
     return (
-        <div className="absolute top-0 right-0 left-0 bg-[#7E33E0] px-32 py-4">
-            <div className="flex flex-row justify-between">
-                <div className="flex flex-row">
+        <div className="mt-16 px-60 py-4 ">
+            <div className="flex flex-row justify-between items-center">
+                <div className="flex flex-row justify-between items-center w-1/2">
                     <div className="flex flex-row">
-                        <span>Hekto</span>
+                        <span className="text-[#0D0E43] text-2xl">Hekto</span>
                     </div>
                     <div className="flex flex-row">
-                        <span>Home</span>
-                        <span>Pages</span>
-                        <span>Products</span>
-                        <span>Blog</span>
-                        <span>Shop</span>
-                        <span>Contact</span>
+                        <span className="text-[#0D0E43] mx-2">Home</span>
+                        <span className="text-[#0D0E43] mx-2">Pages</span>
+                        <span className="text-[#0D0E43] mx-2">Products</span>
+                        <span className="text-[#0D0E43] mx-2">Blog</span>
+                        <span className="text-[#0D0E43] mx-2">Shop</span>
+                        <span className="text-[#0D0E43] mx-2">Contact</span>
                     </div>
                 </div>
                 <div className="flex flex-row">
                     <div className="flex flex-row">
-                        <input type="text" onChange={(ev) => setSearchValue(ev.value)} value={searchValue} />
-                        <button className="my-0 mx-auto p-2 bg-[#FB2E86] rounded">
-                            <AiOutlineSearch />
+                        <input
+                            type="text"
+                            className="rounded-l border-t-2 border-l-2 border-b-2 border-slate-300 w-60"
+                            onChange={(ev) => setSearchValue(ev.value)}
+                            value={searchValue}
+                        />
+                        <button className="my-0 mx-auto p-2 bg-[#FB2E86] rounded-r my-[1px]">
+                            <AiOutlineSearch className="text-white" />
                         </button>
                     </div>
                 </div>
--- src/components/navbars/SecondaryNav.js
+++ src/components/navbars/SecondaryNav.js
@@ -1,36 +1,37 @@
 import React from "react";
 import { AiOutlineMail, AiOutlineShoppingCart } from "react-icons/ai";
 import { BiPhoneCall } from "react-icons/bi";
+import { BsArrowDown } from "react-icons/bs";
 
 const SecondaryNav = () => {
     return (
-        <div className="absolute top-0 right-0 left-0 bg-[#7E33E0] px-32 py-4">
-            <div className="flex flex-row justify-between">
-                <div className="flex flex-row">
-                    <div className="flex flex-row">
-                        <AiOutlineMail />
-                        <span>ashrafraneem@gmail.com</span>
+        <div className="absolute top-0 right-0 left-0 bg-[#7E33E0] px-60 py-4">
+            <div className="flex flex-row items-center justify-between">
+                <div className="flex flex-row items-center">
+                    <div className="flex flex-row items-center">
+                        <AiOutlineMail size={18} className="text-[#f1f1f1]" />
+                        <span className="text-[#f1f1f1] mr-2 ml-1">ashrafraneem@gmail.com</span>
                     </div>
-                    <div className="flex flex-row">
-                        <BiPhoneCall />
-                        <span>888-888-8888</span>
+                    <div className="flex flex-row items-center">
+                        <BiPhoneCall className="text-[#f1f1f1] mb-[2px]" />
+                        <span className="text-[#f1f1f1] mr-2 ml-1">888-888-8888</span>
                     </div>
                 </div>
                 <div className="flex flex-row">
                     <div className="flex flex-row">
-                        <AiOutlineMail />
-                        <span>English</span>
+                        <span className="text-[#f1f1f1] ml-2">English</span>
+                        <BsArrowDown className="text-[#f1f1f1] mt-[0.8px]" />
                     </div>
                     <div className="flex flex-row">
-                        <BiPhoneCall />
-                        <span>Login</span>
+                        <span className="text-[#f1f1f1] ml-2 mr-1">Login</span>
+                        <BiPhoneCall className="text-[#f1f1f1] mt-[0.8px]" />
                     </div>
                     <div className="flex flex-row">
-                        <AiOutlineMail />
-                        <span>Wishlist</span>
+                        <span className="text-[#f1f1f1] ml-2 mr-1">Wishlist</span>
+                        <AiOutlineMail className="text-[#f1f1f1] mt-[3px]" />
                     </div>
                     <div className="flex flex-row">
-                        <AiOutlineShoppingCart />
+                        <AiOutlineShoppingCart size={22} className="text-[#f1f1f1] ml-6" />
                     </div>
                 </div>
             </div>
